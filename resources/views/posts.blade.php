17e195cabb5ae15c01761f3614fd8540bd956192
Nii797 <Phanterasonpardus797@gmail.com>
2021-12-03 02:24:49 +0700

    modifikasi post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,10 +7,10 @@
     @foreach ($posts as $post)
         <article class="mt-4 mb-5">
             <h2>
-                <a href="/posts/{{ $post['id'] }}">{{ $post["title"] }}</a>
+                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
             </h2>
-            <h5>By: {{ $post["author"] }}</h5>
-            <p>{{ $post["body"] }}</p>
+            <h5>By: </h5>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 
