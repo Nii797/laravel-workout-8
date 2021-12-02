9e573b8a296de590e28d867e2cbd4c775936e974
Nii797 <Phanterasonpardus797@gmail.com>
2021-12-03 02:16:12 +0700

    edit post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // mengakses hanya dalam field dalam array nya saja
+    protected $fillable = [ 'title', 'excerpt', 'body' ];
+    // mengisi semua kecuali id
+    protected $guarded = ['id'];
 }
