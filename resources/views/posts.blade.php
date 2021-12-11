d354feffd5c4f9af35eb9c772c78b8023e9ae4f7
Nii797 <phanterasonpardus797@gmail.com>
2021-12-11 22:32:10 +0700

    modification post blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,19 @@
 
 @section('container')
 
-    <h1 class="mt-4">Halaman Blog Post</h1>
+    <h1 class="mt-4 mb-5">Halaman Blog Post</h1>
 
     @foreach ($posts as $post)
-        <article class="mt-4 mb- 5">
+        <article class="mb- 5 border-bottom pb-5">
             <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <h5>By: </h5>
+
+            <p>By Soni Hidayatulloh in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
             <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more ...</a>
         </article>
     @endforeach
 
