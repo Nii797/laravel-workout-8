1b296cb004ff8f00edd3d4aace3def82ed8362f1
Nii797 <phanterasonpardus797@gmail.com>
2021-12-14 11:24:53 +0700

    modifikasi post blade, posts blade, web route

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 
     <h2>{{ $post->title }}</h2>
 
-    <p>By. <a href="">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,9 +11,7 @@
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
 
-            <p>By. <a href="#">{{ $post->user->name }}</a> | <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-
-            {{-- <p>By. <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p> --}}
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> | <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
             <p>{!! $post->excerpt !!}</p>
 
